b2a9fe77b6d616a158fb3dfddbc1c7c820ea3aa3
Marek Zelinka <mzelinka17@gmail.com>
2023-02-19 18:55:19 +0100

    init test database

--- tests/note-api.test.js
+++ tests/note-api.test.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 import supertest from "supertest";
-import { afterAll, expect, test } from "vitest";
-import { app } from "../src/app";
+import { afterAll, beforeEach, expect, test } from "vitest";
+import { app } from "../src/app.js";
+import { Note } from "../src/models/note.js";
 
 const api = supertest(app);
+
+const mockNotes = [
+  { content: "HTML is easy" },
+  { content: "Browsers can execute only JavaScript", important: true },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany();
+  await Note.insertMany(mockNotes);
+});
 
 test("notes are returned as json", async () => {
   const response = await api.get("/api/notes");
@@ -12,16 +23,17 @@ test("notes are returned as json", async () => {
   expect(response.headers["content-type"]).toMatch(/application\/json/);
 });
 
-test("there are two notes", async () => {
+test("all notes are returned", async () => {
   const response = await api.get("/api/notes");
 
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(mockNotes.length);
 });
 
-test("the first note is about HTML", async () => {
+test("a specific note is within the returned notes", async () => {
   const response = await api.get("/api/notes");
 
-  expect(response.body[0].content).toBe("HTML is Easy");
+  const contents = response.body.map((note) => note.content);
+  expect(contents).toContain(mockNotes[1].content);
 });
 
 afterAll(() => {
