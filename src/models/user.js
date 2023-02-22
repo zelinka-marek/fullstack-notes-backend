aafd146abfc0e83cd455d0bfc250dbeb0d5d8ca8
Marek Zelinka <mzelinka17@gmail.com>
2023-02-22 13:10:11 +0100

    validate user

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,23 @@ import uniqueValidator from "mongoose-unique-validator";
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    minLength: [4, "Username must be at least 4 characters long"],
+    required: [true, "User name is required"],
+    trim: true,
     unique: true,
+    validate: {
+      validator: (value) =>
+        /^[a-z._]+$/i.test(value) &&
+        !value.startsWith(".") &&
+        !value.startsWith("_") &&
+        !value.endsWith(".") &&
+        !value.endsWith("_") &&
+        !value.includes("..") &&
+        !value.startsWith("__") &&
+        !value.startsWith("._") &&
+        !value.startsWith("_."),
+      message: "Username must contain only permitted characters",
+    },
   },
   name: String,
   passwordHash: String,
