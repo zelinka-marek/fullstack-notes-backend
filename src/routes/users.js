0564fea309505167826277a6197bec0143d6a1b2
Marek Zelinka <mzelinka17@gmail.com>
2023-02-23 09:34:17 +0100

    populate notes

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,10 @@ import { User } from "../models/user.js";
 export const usersRouter = express.Router();
 
 usersRouter.get("/", async (_request, response) => {
-  const users = await User.find();
+  const users = await User.find().populate("notes", {
+    content: 1,
+    important: 1,
+  });
 
   response.json(users);
 });
